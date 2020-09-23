9a8dd9467c7d21dd61372189c74bd6e1a3b9cd08
mathias <mathias.rahikainen@gmail.com>
2020-09-23 23:47:37 +0300

    updated cloud function so that it validates images from the storage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,12 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+// import * as admin from "firebase-admin";
+// const serviceAccount = require("../../keys/landmark_key.json");
+/*
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://landmark-e738f.firebaseio.com",
+});
+*/
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +15,24 @@ import * as functions from 'firebase-functions';
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.validateLandmark = functions.https.onRequest(async (req, res) => {
+  const vision = require("@google-cloud/vision");
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+  try {
+    // Performs label detection on the image file
+    const bucketName = "landmark-e738f.appspot.com/uploads";
+    const fileName = "photo.jpg";
+    const [result] = await client.landmarkDetection(
+      `gs://${bucketName}/${fileName}`
+    );
+    const landmarks = result.landmarkAnnotations;
+    console.log("Landmarks:");
+    landmarks.forEach((landmark: any) => console.log(landmark));
+    res.send(landmarks);
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+});
