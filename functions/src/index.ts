5bc0a728a6902f60bfc77b300242e649bd7880e0
mathias <mathias.rahikainen@gmail.com>
2020-11-03 14:13:44 +0200

    update cloud function on article branch

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,33 +1,19 @@
 import * as functions from "firebase-functions";
-/*import * as admin from "firebase-admin";
-const serviceAccount = require("../../keys/landmark_key.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://landmark-e738f.firebaseio.com",
-});
-*/
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import vision from "@google-cloud/vision";
 
 exports.validateLandmark = functions.https.onRequest(async (req, res) => {
-  const vision = require("@google-cloud/vision");
   // Creates a client
   const client = new vision.ImageAnnotatorClient();
   try {
     console.log(req.body);
     // Performs label detection on the image file
-    const bucketName = "landmark-e738f.appspot.com/uploads";
+    const projectId = "landmark-bff66";
+    const bucketName = `${projectId}.appspot.com/uploads`;
     const fileName = `${req.body.photoId}`;
     const [result] = await client.landmarkDetection(
       `gs://${bucketName}/${fileName}`
     );
-    const landmarks = result.landmarkAnnotations;
+    const landmarks = result.landmarkAnnotations || [];
     console.log("Landmarks:");
     landmarks.forEach((landmark: any) => console.log(landmark));
     res.send(landmarks);
