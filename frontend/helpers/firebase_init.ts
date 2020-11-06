d11550cdc427c744aa96dc5bfdcdf394bc4148b2
mathias <mathias.rahikainen@gmail.com>
2020-11-06 10:15:02 +0200

    refactor firebase_init.ts

--- frontend/helpers/firebase_init.ts
+++ frontend/helpers/firebase_init.ts
@@ -2,14 +2,14 @@ import * as firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/auth";
 import "firebase/firestore";
-
+const projectId = "landmark-e738f";
 const firebaseConfig = {
   apiKey: "AIzaSyAr0T4Zf5TaqZJcz86y_XLwIQX5b3epkXM",
-  authDomain: "landmark-e738f.firebaseapp.com",
-  databaseURL: "https://landmark-e738f.firebaseio.com",
-  projectId: "landmark-e738f",
-  storageBucket: "landmark-e738f.appspot.com",
-  appId: "534472241145",
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  projectId: `${projectId}`,
+  storageBucket: `${projectId}.appspot.com`,
+  appId: "534472241145", // "Project number" in Firebase Console
   measurementId: "G-MEASUREMENT_ID",
 };
 const Firebase = firebase.initializeApp(firebaseConfig);
